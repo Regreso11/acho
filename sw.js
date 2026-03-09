a01fc2cbf3cd80d8ec9e6e42d76a78262fa89eca
Regreso11 <regresodios@gmail.com>
2026-03-09 14:56:33 -0400

    Update service worker to version 6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_VERSION = "v5"; // <-- cambia a v3, v4, etc. por release
+const CACHE_VERSION = "v6"; // <-- cambia a v3, v4, etc. por release
 const CACHE_NAME = `flashcards-${CACHE_VERSION}`;
 
 const CORE_ASSETS = [
@@ -93,6 +93,7 @@ async function cacheFirst(req) {
   cache.put(req, fresh.clone());
   return fresh;
 }
+
 
 
 
